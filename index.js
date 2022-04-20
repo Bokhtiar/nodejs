57e118b72adc289401ae7f6e09f468df5dca5ed1
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-20 12:58:17 +0600

    nodemon package and route create

--- index.js
+++ index.js
@@ -2,7 +2,19 @@
 const express = require('express')
 
 const app = express()
-const POST = process.env.PORT || 3000
-app.listen(POST , () => {
+const PORT = process.env.PORT || 3000
+
+
+
+app.get('/', (req, res) => {
+    res.send('<h3>HELLOW WasdasdORLDasdfasdfa sdf </h3>')
+})
+
+app.get('/post', (req, res) => {
+    res.send('post page')
+})
+
+
+app.listen(PORT , () => {
     console.log(`server is running ${PORT}`)
 })
