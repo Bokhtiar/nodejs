bbbaf36a350ea0f83e548a598d14586320e7971a
bokhtiar <bokhtiartoshar1@gmail.com>
2022-04-20 14:51:22 +0600

    node js update

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 //server.js 
 const express = require('express')
+var bodyParser = require('body-parser')
+
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+const contacts = []
+
 const PORT = process.env.PORT || 3000
 
+const contactRoute = require('./api/routes/contact')
+
+
+
+app.use((req, res, next) => {
+    console.log('middleware')
+    next()
+})
 
 
 app.get('/', (req, res) => {
     res.send('<h3>HELLOW WasdasdORLDasdfasdfa sdf </h3>')
 })
 
-app.get('/post', (req, res) => {
-    res.send('post page')
+app.get('/api/contact', (req, res) => {
+    res.status(200).json({
+        contacts
+    })
 })
+app.post('/api/contact/store', (req, res) => {
+    // const name = req.body.name
+    // const email = req.body.email 
 
+    contacts.push({
+        name: req.body.name,
+        email: req.body.email
+    })
+    res.status(201).json({
+        message : 'succeffully data',
+    })
+})
 
 app.listen(PORT , () => {
     console.log(`server is running ${PORT}`)
 })
+
+
+
+const contact = [
+    {name: 'bokhtiar', email : 'bokhtiar@gmail.com'},
+    {name: 'joy', email: 'joy@gmail.com'},
+    {name: 'kawdik', email: 'kawdik@gmail.com'}
+]
